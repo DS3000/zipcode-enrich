0157012938b97a44763d4977a3cb315a16574ae8
DS3000 <davidsa3000@gmail.com>
2024-10-09 10:13:59 +0100

    Added mysql connector. Added queries to the database (insertion and fetching).
    Added Flask to launch a server which responds to HTTP requests, in order to make the enriched data available.
    
    Added csv dumping function to save the enriched data.

--- main.py
+++ main.py
@@ -1,13 +1,100 @@
 import csv
+import mysql.connector
 from datetime import datetime, UTC
-from typing import List
+from flask import Flask, request, jsonify
+from typing import List, Dict
 
+import my_secrets
 from CodigoPostalPtExtractor import CodigoPostalPtExtractor
 from CttCodigoPostalExtractor import CttCodigoPostalExtractor
 from HtmlCache import HtmlCache
 from LocaleInfo import LocaleInfo
 from ZipInfoExtractor import ZipInfoExtractor
 from ZipcodeInfo import ZipcodeInfo
+
+cnx = mysql.connector.connect(user='root', database='zipcode_db', host='localhost', password=my_secrets.db_password)
+
+
+def db_search_for_zipcode(zip_info: ZipcodeInfo) -> LocaleInfo | None:
+    with cnx.cursor() as cursor:
+        query = (
+            "select t_concelho.name, t_distrito.name, t_infomap.zip from t_infomap"
+            " left join t_concelho on t_concelho.id_concelho=t_infomap.id_concelho"
+            " left join t_distrito on t_distrito.id_distrito=t_infomap.id_distrito"
+            f" where t_infomap.zip='{zip_info}'"
+        )
+        cursor.execute(query)
+        print("Query results:")
+
+        for concelho, distrito, zipcode in cursor:
+            print(f"Concelho: {concelho}, Distrito: {distrito}, zipcode: {zipcode}")
+            return LocaleInfo(distrito, concelho)
+
+    return None
+
+
+flask_app: Flask = Flask(__name__)
+
+html_cache: HtmlCache = HtmlCache('cache')
+
+extractors: List[ZipInfoExtractor] = [
+    CodigoPostalPtExtractor(html_cache),
+    CttCodigoPostalExtractor(html_cache)
+]
+
+info_map: Dict[ZipcodeInfo, LocaleInfo] = {}
+
+
+@flask_app.route('/', methods=['GET'])
+def get_locale_info():
+    param_name = 'codigo_postal'
+
+    codigo_postal = request.args.get(param_name)
+
+    if codigo_postal is None:
+        msg = (
+            f'Missing param {param_name}. Format must be <cp4>-<cp3>,'
+            ' where cp4 is four digits, and cp3 is three digits'
+        )
+        return (
+            jsonify({'message': msg}),
+            400
+        )
+
+    try:
+        parts = codigo_postal.split('-')
+
+        # make sure they're valid numbers
+        cp4 = int(parts[0])
+        cp3 = int(parts[1])
+
+        zip_info: ZipcodeInfo = ZipcodeInfo(parts[0], parts[1])
+    except:
+        msg = (
+            f'{codigo_postal} is not a valid zip code. Format must be <cp4>-<cp3>,'
+            ' where cp4 is four digits, and cp3 is three digits'
+        )
+        return (
+            jsonify({'message': msg}),
+            400
+        )
+
+    print(f"Request made, with info {zip_info}")
+
+    res = info_map.get(zip_info)
+    # res = db_search_for_zipcode(zip_info)
+    print(f"Got {res}")
+
+    if res is None:
+        return (
+            jsonify({'message': f'zip code not in the database'}),
+            404
+        )
+
+    return (
+        jsonify({'concelho': res.concelho, 'distrito': res.distrito}),
+        200
+    )
 
 
 def zipcode_infos_from_csv(filepath: str) -> List[ZipcodeInfo]:
@@ -24,6 +111,49 @@ def zipcode_infos_from_csv(filepath: str) -> List[ZipcodeInfo]:
         return result
 
 
+def dump_infos_to_csv(filepath: str, infos: Dict[ZipcodeInfo, LocaleInfo]):
+    with open(filepath, 'w', newline='', encoding='utf8') as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow(["cp7", "concelho", "distrito"])
+        for zip_info, locale_info in infos.items():
+            csv_writer.writerow([f"{zip_info.cp4}-{zip_info.cp3}", locale_info.concelho, locale_info.distrito])
+
+
+def insert_into_db(info_map: Dict[ZipcodeInfo, LocaleInfo]):
+    insert_into_concelho_query = "insert into t_concelho (name) values(%(concelho_name)s)"
+    insert_into_distrito_query = "insert into t_distrito (name) values(%(distrito_name)s)"
+    insert_into_infomap_query = ("insert into t_infomap (id_concelho, id_distrito, zip)"
+                                 " values(%(id_concelho)s, %(id_distrito)s, %(zip_code)s)")
+
+    for zipcode_info, locale_info in info_map.items():
+        with cnx.cursor() as cursor:
+            try:
+                cursor.execute(insert_into_concelho_query, {"concelho_name": locale_info.concelho})
+                id_concelho = cursor.lastrowid
+
+                cursor.execute(insert_into_distrito_query, {"distrito_name": locale_info.distrito})
+                id_distrito = cursor.lastrowid
+
+                data = {
+                    'id_distrito': id_distrito,
+                    'id_concelho': id_concelho,
+                    'zip_code': str(zipcode_info)
+                }
+
+                cursor.execute(insert_into_infomap_query, data)
+
+                cnx.commit()
+            except mysql.connector.Error as e:
+                # TODO: handle duplicate entries
+                print("Error code:", e.errno)  # error number
+                print("SQLSTATE value:", e.sqlstate)  # SQLSTATE value
+                print("Error message:", e.msg)  # error message
+                print("Error:", e)  # errno, sqlstate, msg values
+                s = str(e)
+                print("Error:", s)  # errno, sqlstate, msg values
+
+
 def main():
     csv_file_path = r"codigos_postais.csv"
     failed_requests_log_filepath = 'failed.txt'
@@ -33,17 +163,11 @@ def main():
     failed_requests_fp.write("\n\n")
     missing_data_fp = open(missing_data_log_filepath, 'a')
     missing_data_fp.write("\n\n")
-    html_cache: HtmlCache = HtmlCache('cache')
-
-    extractors: List[ZipInfoExtractor] = [
-        CodigoPostalPtExtractor(html_cache),
-        CttCodigoPostalExtractor(html_cache)
-    ]
 
     infos = zipcode_infos_from_csv(csv_file_path)
 
     slice_start_idx = 0
-    slice_end_idx = 100
+    slice_end_idx = 200
 
     infos_slice = infos[slice_start_idx: slice_end_idx]
 
@@ -65,10 +189,20 @@ def main():
             missing_data_fp.write(f"{datetime.now(UTC)} - {zip_info}\n")
             continue
 
+        info_map[zip_info] = res
         print(f"Got {res}")
 
     missing_data_fp.close()
     failed_requests_fp.close()
+
+    dump_infos_to_csv('enriched.csv', info_map)
+
+    insert_into_db(info_map)
+
+    print("Serving API now...")
+    flask_app.run()
+
+    cnx.close()
 
 
 if __name__ == '__main__':
