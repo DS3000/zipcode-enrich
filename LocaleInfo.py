97c39208eaebe665f33e27c96eca4412e150fc53
DS3000 <davidsa3000@gmail.com>
2024-10-09 10:10:15 +0100

    LocaleInfo now uses lowercase for fields distrito and concelho.

--- LocaleInfo.py
+++ LocaleInfo.py
@@ -1,10 +1,10 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class LocaleInfo:
     distrito: str
     concelho: str
+
+    def __init__(self, distrito: str, concelho: str):
+        self.distrito = distrito.lower()
+        self.concelho = concelho.lower()
 
     def __str__(self):
         return f"Distrito: {self.distrito}; Concelho: {self.concelho}"
