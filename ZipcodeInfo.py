7023bfbf03d304e224581eb3ff1b0bc5cd6bf2ef
DS3000 <davidsa3000@gmail.com>
2024-10-09 10:07:05 +0100

    Made changes to make ZipcodeInfo hashable.

--- ZipcodeInfo.py
+++ ZipcodeInfo.py
@@ -8,3 +8,13 @@ class ZipcodeInfo:
 
     def __str__(self):
         return f"{self.cp4}-{self.cp3}"
+
+    def __eq__(self, other):
+        return (
+                isinstance(other, self.__class__) and
+                self.cp4 == other.cp4 and
+                self.cp3 == other.cp3
+        )
+
+    def __hash__(self):
+        return hash(f"{self.cp4}-{self.cp3}")
