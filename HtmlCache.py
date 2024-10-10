35095f4d6c0843b6ed7c864d280e1d47c5c01568
DS3000 <davidsa3000@gmail.com>
2024-10-10 09:54:48 +0100

    Added error handling in case cache file path terminates in a directory path delimiter.
    Added behavior to create directories in the cache file path as needed.

--- HtmlCache.py
+++ HtmlCache.py
@@ -1,3 +1,4 @@
+import os.path
 from os import path
 from time import sleep
 
@@ -13,6 +14,9 @@ class HtmlCache:
         return self.__cache_base_dir
 
     def get(self, url: str, cache_file_path: str) -> str | None:
+        if cache_file_path.endswith('/') or cache_file_path.endswith('\\'):
+            raise ValueError(
+                "cache_file_path must be a file path, and not end in directory path delimiter (ambiguity: would this be a file path or a dir path?)")
 
         if path.exists(f'{cache_file_path}'):
             with open(cache_file_path, 'r', encoding='utf8') as fp:
@@ -27,7 +31,11 @@ class HtmlCache:
         if req.status_code != 200:
             return None
 
-        # TODO: if the basedir path doesn't exist, create it
+        # create dirs as needed
+        cache_file_path_dirname = os.path.dirname(cache_file_path)
+        if not os.path.exists(cache_file_path_dirname):
+            os.makedirs(cache_file_path_dirname)
+
         with open(f'{cache_file_path}', 'w', encoding='utf8') as fp:
             fp.write(req.text)
 
